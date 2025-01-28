7f25b560981669a74483500adbf054c4b4acd4e4
vaib01232 <119183394+vaib01232@users.noreply.github.com>
2025-01-28 09:17:07 +0530

    Updated code

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const apikey = process.env.API_KEY;
 const secret = process.env.SERVER_SECRET || 'My self';
-const kalvian = process.env.IS_KALVIAN === 'true';
+const kalvian = process.env.IS_KALVIAN === 'false';
 // const kalvian = true;
 // const apiKey = '289fa828-ebb6-405a-bdb1-bf2d16ec8d66';
 // const secret = 'server_secret';
